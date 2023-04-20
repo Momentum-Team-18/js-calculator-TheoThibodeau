171e93c537e9b13d8d8830a76b5248bebed128e0
TheoThibodeau <teddythibodeau@gmail.com>
2023-04-20 17:34:00 -0400

    numbers show up on calc

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-let clearButton =
-document.querySelector("#clearButton");
-console.log (clearButton)
+//define variables
+let clearButton = document.querySelector("#clearButton");
+let numButtons = document.querySelectorAll(".numButton");
+let outPut = document.querySelector("#outPut")
+let equal = document.querySelector("#equal")
 
+//register that they work and arre being targeted
 clearButton.addEventListener('click', (event) => {
     console.log (event.target)
 });
 
-let numButtons = 
-document.querySelectorAll(".numButton");
+equal.addEventListener('click', (event) => {
+    console.log (event.target);
+});
 
-for (let button of numButtons) {
-    button.addEventListener('click', (event) => {
-        console.log (event.target)
-    });
+outPut.addEventListener('click', (event) => {
+    console.log (event.target);
+});
+
+//functions
+const calculate = function(formula){
+    let result = eval(formula)
+    outPut.value = result
 }
 
 
+//event listeners
+for (let button of numButtons) {
+    button.addEventListener('click', (event) => {
+        outPut.value += button.innerText
+    });
+}
 
-// for (let button of numButtons) {
-//     button.addEventListener("click", (event) => {
-//         display (event.target.value)
-//     })
-// }
-
-// for (let button of output) {
-//     button.addEventListener("click", (event) =>
-//     {
-//         console.log(event.target.innerText);
-//     });
-//     featureDisplay.innerText =
-//     even.target.innerText
-// }
+// for (let clear of clearButton) why should you not use a for loop here?
+    clearButton.addEventListener('click', (event) => {
+        outPut.value += '  ';
+    });    
